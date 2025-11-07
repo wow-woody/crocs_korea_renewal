04cd57f73373f1e11b82ee91c88b9e99ecc28fb9
Lim Chae A <imchae4@gmail.com>
2025-11-07 16:10:13 +0900

    상단 팝업 창 작업

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MainSlider from '../components/MainSlider';
+import TopPopup from '../components/TopPopup';
 
 const Main = () => {
     return (
         <main>
             <MainSlider />
+            <TopPopup />
             <div className="container"></div>
         </main>
     );
