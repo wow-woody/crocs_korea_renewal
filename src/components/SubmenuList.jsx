e59c0704f487a33de0b9b43c05507db05e512fd6
Lim Chae A <imchae4@gmail.com>
2025-11-09 20:40:51 +0900

    서브메뉴 수정

--- src/components/SubmenuList.jsx
+++ src/components/SubmenuList.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+const submenus = [
+    { key: 'new', label: 'NEW', imgUrl: './images/item_01.png' },
+    { key: 'women', label: 'WOMEN', imgUrl: './images/item_01.png' },
+    { key: 'men', label: 'MEN', imgUrl: './images/item_01.png' },
+    { key: 'kids', label: 'KIDS', imgUrl: './images/item_01.png' },
+    { key: 'jibbitz', label: 'JIBBITZ', imgUrl: './images/item_01.png' },
+    { key: 'collabs', label: 'COLLABS', imgUrl: './images/item_01.png' },
+];
 
 const SubmenuList = () => {
     return (
         <div className="submenu_wrap">
-            <ul className="submenu"></ul>
+            <ul className="submenu">
+                {submenus.map((submenu) => (
+                    <li key={submenu.key}>
+                        <Link to={`/${submenu.key}`}>
+                            <img src={submenu.imgUrl} alt={submenu.key} />
+                            <span>{submenu.label}</span>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
