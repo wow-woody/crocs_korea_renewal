dcbaf270bedcc0bfc60fbb5ded193a17d6db922d
Lim Chae A <imchae4@gmail.com>
2025-11-15 17:40:57 +0900

    로그인 페이지 이미지 변경

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -50,13 +50,25 @@ const Login = () => {
                     <Title subTitle="CrocsClub Benefit" />
                     <ul className="benefit_list">
                         <li>
-                            <img src="./images/benefit_img_01.svg" alt="benefit" />
+                            <span>
+                                <img src="./images/benefit_img_01.svg" alt="benefit" />
+                            </span>
+                            첫 주문 <br />
+                            추가 15% 할인
                         </li>
                         <li>
-                            <img src="./images/benefit_img_02.svg" alt="benefit" />
+                            <span>
+                                <img src="./images/benefit_img_02.svg" alt="benefit" />
+                            </span>
+                            온라인 단독 <br />
+                            할인 & 행사
                         </li>
                         <li>
-                            <img src="./images/benefit_img_03.svg" alt="benefit" />
+                            <span>
+                                <img src="./images/benefit_img_03.svg" alt="benefit" />
+                            </span>
+                            VIP 세일 & <br />
+                            프라이빗 이벤트
                         </li>
                     </ul>
                     {/* <button>CrocsClub Join</button> */}
