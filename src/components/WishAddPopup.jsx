554c2f77ee9b8999da83977737d60d62707996cd
chowon <zzzeus9876@gmail.com>
2025-11-16 17:39:02 +0900

    feat: 위시팝업 <-> 위시리스트 페이지 이동 연결

--- src/components/WishAddPopup.jsx
+++ src/components/WishAddPopup.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { wishListStore } from '../store/wishListStore';
+import { Link } from 'react-router-dom';
 
 const WishAddPopup = () => {
     const { popUp, hidePopup } = wishListStore();
@@ -10,7 +11,7 @@ const WishAddPopup = () => {
         <div className="wish_popup_wrap">
             <div className="popup">{popUp.message}</div>
             <button onClick={hidePopup}>쇼핑 계속하기</button>
-            <button>위시리스트 이동</button>
+            <Link to="/wishlist">위시리스트 이동</Link>
         </div>
     );
 };
--- src/pages/JibbitzCollaboProductDetail.jsx
+++ src/pages/JibbitzCollaboProductDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { collaboAuthStore } from '../store/collaboAuthStore';
 import { wishListStore } from '../store/wishListStore';
 import Title from '../components/Title';
@@ -10,7 +10,6 @@ const JibbitzCollaboProductDetail = () => {
     const { id } = useParams();
     const { disneyItems } = collaboAuthStore();
     const { onAddWishList } = wishListStore();
-    console.log('jibbitzItems:', disneyItems);
 
     //찾은 상품을 저장할 변수
     const [product, setProduct] = useState(null);
@@ -19,13 +18,11 @@ const JibbitzCollaboProductDetail = () => {
         if (!id || disneyItems.length === 0) return;
         //뿌려질 제품 찾기
         const findItem = disneyItems.find((item) => String(item.id) === String(id));
-        console.log('findItem:', findItem);
         setProduct(findItem);
-        console.log('들어왔나?');
     }, [id, disneyItems]);
 
     if (!product) {
-        return <div>상품 정보를 불러오고 있으니 기다리라 ㅡㅡ </div>;
+        return <div>상품 정보를 불러오고 있으니 기다리</div>;
     }
 
     return (
@@ -36,14 +33,10 @@ const JibbitzCollaboProductDetail = () => {
                     <p>상품 예시입니당 💚</p>
                     <button onClick={() => onAddWishList(product)}>위시버튼💚</button>
                     <WishAddPopup />
-                    <Link to="/wishlist" product={product}>
-                        위시리스트 이동
-                    </Link>
                     <JibbitzCollaboProductDetailCard product={product} />
                 </div>
             </div>
         </div>
     );
 };
-console.log('왜 안나오니??');
 export default JibbitzCollaboProductDetail;
