5ae91c050b5bf5d32b2c1c00860f3929db2d31e0
jiin <amesystand@gmail.com>
2025-11-18 18:33:08 +0900

    feat: 신상품 출시예정만 필터링

--- src/store/useNewProductStore.js
+++ src/store/useNewProductStore.js
@@ -16,10 +16,9 @@ export const useNewProductStore = create((set, get) => ({
                 const cateLower = product.cate.toLowerCase();
                 return cateLower.includes("신상품") || cateLower.includes("출시 예정");
         });
+        // console.log(`전체상품: ${Prouducts.length}, 신상품 출시예정 상품: ${filteredProuducts.length}`);
 
-        console.log("전체상품: ${Prouducts.length}, 신상품 및 출시예정 상품: ${filteredProuducts.length}");
-
-        set({ items: Prouducts });
+        set({ items: filteredProuducts});
     },
 }));
 
