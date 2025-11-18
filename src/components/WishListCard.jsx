69105c44a97929d4d69507e900f91eca180dd255
chowon <zzzeus9876@gmail.com>
2025-11-18 15:00:14 +0900

    위시리스트 카드 수정

--- src/components/WishListCard.jsx
+++ src/components/WishListCard.jsx
@@ -7,10 +7,10 @@ import './scss/wishlistcard.scss';
 const WishListCard = () => {
     const { wishLists, onRemoveWish } = wishListStore();
     return (
-        <ul className="wish_card_wrap">
-            {wishLists.map((item) => (
-                <li key={item.id}>
-                    <div className="wish_card">
+        <div className="inner_wrap">
+            <div className="wish_card_wrap">
+                {wishLists.map((item) => (
+                    <div key={item.id} className="wish_card">
                         <div className="wish_card_imgbox">
                             <img src={item.imageUrl} alt={item.title} />
                         </div>
@@ -26,12 +26,16 @@ const WishListCard = () => {
                                 </p>
                             </div>
                         </div>
-                    </div>
-                    {/* <button>장바구니 추가</button>
+                        {/* <button>장바구니 추가</button>
                     <button onClick={() => onRemoveWish(item)}>삭제</button> */}
-                </li>
-            ))}
-        </ul>
+                    </div>
+                ))}
+                <div className="wish_select">
+                    <button className="wish_remove_btn">선택상품 삭제</button>
+                    <button className="wish_add_btn">장바구니 추가</button>
+                </div>
+            </div>
+        </div>
     );
 };
 
