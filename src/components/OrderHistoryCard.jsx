32acfdef140d21b7fe35a6f4a47267be4c184340
chowon <zzzeus9876@gmail.com>
2025-11-19 17:49:18 +0900

    feat: 주문내역 카드 추가

--- src/components/OrderHistoryCard.jsx
+++ src/components/OrderHistoryCard.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import './scss/orderhistory.scss';
 
 const OrderHistoryCard = () => {
     return (
-        <div className="order_list_wrap">
-            <div className="order-list-top"></div>
-            <ul className="order-list">
-                <li>map돌릴것</li>
-            </ul>
+        <div className="order_list_card">
+            {/* map 돌릴 것 */}
+            <div className="order_list_top">
+                <div className="order_list_top_left">
+                    <p>주문날짜</p>
+                    <p>주문번호</p>
+                </div>
+                <div className="order_list_top_right">
+                    <button>상세보기</button>
+                </div>
+            </div>
+            {/* map 돌릴 것 */}
+            <div className="order_list_middle">
+                <div className="order_list_imgbox">
+                    <img src="/images/Category-list__items_01.png" alt="" />
+                </div>
+                <div className="order_list_textbox">
+                    <p>상품명</p>
+                    <p>가격 / 개수</p>
+                </div>
+            </div>
+            <div className="order_list_bottom">
+                <div>주문상태 </div>
+            </div>
         </div>
     );
 };
