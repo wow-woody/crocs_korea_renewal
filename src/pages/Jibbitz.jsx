f8aef3cc502308f8fee61a1432209a48c524fa84
jiin <amesystand@gmail.com>
2025-11-25 17:42:09 +0900

    comb 메인에서 지비츠 합치기

--- src/pages/Jibbitz.jsx
+++ src/pages/Jibbitz.jsx
@@ -3,6 +3,9 @@ import Title from "../components/Title";
 import JibbitzProductListPage from "../components/JibbitzProductListPage";
 import "./scss/productListpage.scss";
 import { useRecentProductsStore } from "../store/recentProductsStore";
+import React from 'react';
+import Title from '../components/Title';
+// import JibbitzProductListPage from '../components/JibbitzProductListPage';
 
 const Jibbitz = () => {
   const { addProduct } = useRecentProductsStore();
@@ -18,6 +21,14 @@ const Jibbitz = () => {
       </div>
     </div>
   );
+    return (
+        <div className="sub_page">
+            <div className="inner">
+                <Title title="jibbitz" />
+                {/* <JibbitzProductListPage /> */}
+            </div>
+        </div>
+    );
 };
 
 export default Jibbitz;
