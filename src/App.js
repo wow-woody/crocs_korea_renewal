58255a2c052e2e83f44008091a0152f1d3da9cbf
wow-woody <jjeongwuss1@gmail.com>
2025-11-26 22:36:35 +0900

    2025-11-26(수)  워크 트리 머지 - v01

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import "./App.scss";
 import { useEffect, useState } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
@@ -23,6 +24,30 @@ import JibbitzProductDetail from "./pages/JibbitzProductDetail";
 import JibbitzProductListPage from "./pages/JibbitzProductListPage";
 import Cart from "./pages/Cart";
 import Order from "./components/Order/Order";
+=======
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+import New from './pages/New';
+import Women from './pages/Women';
+import Men from './pages/Men';
+import Kids from './pages/Kids';
+import Jibbitz from './pages/Jibbitz';
+import Collabs from './pages/Collabs';
+import Brand from './pages/Brand';
+import Promotion from './pages/Promotion';
+import Login from './pages/Login';
+import Join from './pages/Join';
+import Cart from './pages/Cart';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import CrocsClubPopup from './components/CrocsClubPopup';
+import UserInfo from './pages/UserInfo';
+import JibbitzCollaboProductDetail from './pages/JibbitzCollaboProductDetail';
+import WishList from './pages/WishList';
+import OrderHistory from './pages/OrderHistory';
+import ProductDetail from './pages/ProductDetail';
+>>>>>>> 7d4862a (2025-11-26(수)  워크 트리 머지 - v01)
 
 function App() {
     const { user, loading, checkSession, initAuthListener } = loginAuthStore();
@@ -61,6 +86,7 @@ function App() {
             <RecentSidebar isOpen={isRecentOpen} onClose={() => setIsRecentOpen(false)} />
             <Routes>
                 <Route index element={<Main />} />
+<<<<<<< HEAD
                 <Route path='/store' element={<Store />} />
                 <Route path='/Brand' element={<Brand />} />
                 <Route path='/login' element={<Login />} />
@@ -77,6 +103,25 @@ function App() {
                 <Route path='/jibbitz/:id' element={<JibbitzProductDetail />} />
                 <Route path='/cart' element={<Cart />} />
                 <Route path='/order' element={<Order />} />
+=======
+                <Route path="/new" element={<New />} />
+                <Route path="/women" element={<Women />} />
+                <Route path="/men" element={<Men />} />
+                <Route path="/kids" element={<Kids />} />
+                <Route path="/jibbitz" element={<Jibbitz />} />
+                <Route path="/collabs" element={<Collabs />} />
+                <Route path="/promotion" element={<Promotion />} />
+                <Route path="/Brand" element={<Brand />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/join" element={<Join />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/crocsclub" element={<CrocsClubPopup />} />
+                <Route path="/userinfo" element={<UserInfo />} />
+                <Route path="/product/:id" element={<JibbitzCollaboProductDetail />} />
+                <Route path="/product-detail" element={<ProductDetail />} />
+                <Route path="/wishlist" element={<WishList />} />
+                <Route path="/orderhistory" element={<OrderHistory />} />
+>>>>>>> 7d4862a (2025-11-26(수)  워크 트리 머지 - v01)
             </Routes>
             {/* <Footer /> */}
         </div>
