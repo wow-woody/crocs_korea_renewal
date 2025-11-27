2a7321b7cd4f7fef56cfca9fe49595e1d3e13a52
Lim Chae A <imchae4@gmail.com>
2025-11-27 18:58:54 +0900

    오더 히스토리 값 넘기기 완 / 오더 주문자 정보 불러오기 완 / 디테일 페이지 사이즈 필터링 완

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,41 +12,59 @@ const Header = ({ onCartClick, onRecentClick }) => {
     const searchOpen = useCrocsProductStore((state) => state.searchOpen);
     const onOpenSearch = useCrocsProductStore((state) => state.onOpenSearch);
     const onCloseSearch = useCrocsProductStore((state) => state.onCloseSearch);
-
     const [depthOpen, setDepthOpen] = useState(false);
     const [scrolled, setScrolled] = useState(false); // <- 스크롤 상태
     // const [activeDepthMenu, setActiveDepthMenu] = useState('all');
     const location = useLocation();
     const isSubPage = location.pathname !== '/'; // 예: 메인 페이지가 '/'일 경우
 
+    // useEffect(() => {
+    //     const handleScroll = () => {
+    //         const slider = document.querySelector('.main_slider_wrap'); // 슬라이더 래퍼 클래스
+    //         if (slider) {
+    //             const sliderHeight = slider.offsetHeight;
+    //             if (window.scrollY > sliderHeight) {
+    //                 setScrolled(true);
+    //             } else {
+    //                 setScrolled(false);
+    //             }
+    //         }
+    //     };
+
+    //     window.addEventListener('scroll', handleScroll);
+    //     return () => {
+    //         window.removeEventListener('scroll', handleScroll);
+    //     };
+    // }, []);
+
     useEffect(() => {
         const handleScroll = () => {
-            const slider = document.querySelector('.main_slider_wrap'); // 슬라이더 래퍼 클래스
-            if (slider) {
-                const sliderHeight = slider.offsetHeight;
-                if (window.scrollY > sliderHeight) {
-                    setScrolled(true);
-                } else {
-                    setScrolled(false);
-                }
+            const slider = document.querySelector('.main_slider_wrap');
+            const sliderHeight = slider?.offsetHeight || 1; // 0 방지
+            if (window.scrollY > sliderHeight) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
             }
         };
 
         window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
+        handleScroll(); // 첫 실행
+
+        return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
     return (
         <>
             {/* header와 depth1을 감싸는 wrapper */}
             <div
-                className={`header_wrapper
-                ${depthOpen ? 'open' : ''}
-                ${searchOpen ? 'hide' : ''}
-                ${scrolled ? 'scrolled' : ''}
-                ${isSubPage ? 'subpage' : ''}`}
+                className={[
+                    'header_wrapper',
+                    isSubPage ? 'subpage' : '',
+                    depthOpen ? 'open' : '',
+                    searchOpen ? 'hide' : '',
+                    scrolled ? 'scrolled' : '',
+                ].join(' ')}
             >
                 <header className="header">
                     <div className="wide_inner">
@@ -66,18 +84,19 @@ const Header = ({ onCartClick, onRecentClick }) => {
                         <div className="header_right">
                             <GnbWrap onSearchClick={onOpenSearch} />
                         </div>
-                        <div className="header_slide_buttons">
-                            <button className="cart-button" onClick={onCartClick}>
-                                <img src="./images/cart-slide-icon.svg" alt="카트팝업" />
-                            </button>
-                            <button className="recently-button" onClick={onRecentClick}>
-                                <img src="./images/recently-slide-icon.svg" alt="최근본상품팝업" />
-                            </button>
-                        </div>
                     </div>
                 </header>
 
                 {depthOpen && <Depth1 setDepthOpen={setDepthOpen} />}
+            </div>
+
+            <div className="header_slide_buttons">
+                <button className="cart-button" onClick={onCartClick}>
+                    <img src="/images/cart-slide-icon.svg" alt="카트팝업" />
+                </button>
+                <button className="recently-button" onClick={onRecentClick}>
+                    <img src="/images/recently-slide-icon.svg" alt="최근본상품팝업" />
+                </button>
             </div>
 
             {/* {searchOpen && <Search onClose={onCloseSearch} />} */}
