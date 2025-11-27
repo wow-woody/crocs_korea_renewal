d2bee90a131f0d6b3aeaf44317e251c9b5e40f69
Lim Chae A <imchae4@gmail.com>
2025-11-27 23:29:54 +0900

    브랜드 페이지 글 정리 / 사진 정리 중/오더 페이지 주석 삭제 및 주문 완료 시 데이터 불러오기 등

--- src/pages/CrocsProductDetail.jsx
+++ src/pages/CrocsProductDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useCrocsProductStore } from '../store/useCrocsProductStore';
 import { wishListStore } from '../store/wishListStore';
